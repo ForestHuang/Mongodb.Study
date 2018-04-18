0c4d45700766c2bcda044f4688b2545599359af8
森霖 黄 <huangmumumu@outlook.com>
2018-04-18 17:37:53 +0800

    SURE

---
+++ Mongodb.Common/MongodbHelper.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using MongoDB.Driver.Core;
+
+
+namespace Mongodb.Common
+{
+    public class MongodbHelper<T> where T : class
+    {
+        private static string serverHost = string.Empty;
+        private static string databaseName = string.Empty;
+        private static string collectionName = string.Empty;
+
+        private static readonly object lockMongodb = new object();
+
+        /// <summary>
+        /// 有参构造
+        /// </summary>
+        /// <param name="serverHost">链接地址（serverHost）</param>
+        /// <param name="databaseName">数据库名（dataBase）</param>
+        /// <param name="collectionName">表名（collections）</param>
+        public MongodbHelper(string _serverHost, string _databaseName, string _collectionName)
+        {
+            serverHost = _serverHost;
+            databaseName = _databaseName;
+            collectionName = _collectionName;
+        }
+
+        /// <summary>
+        /// create Mongodb
+        /// </summary>
+        /// <returns>MongoDatabase</returns>
+        private static IMongoDatabase GetMongodbDataBase() { return new MongoClient(serverHost).GetDatabase(databaseName); }
+
+        /// <summary>
+        /// Insert 
+        /// </summary>
+        /// <param name="entity">数据库对象</param>
+        public void Insert(T entity)
+        {
+            try
+            {
+                var collection = GetMongodbDataBase().GetCollection<T>(collectionName);
+                collection.InsertOne(entity);
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+
+        public List<T> FindAll()
+        {
+            try
+            {
+                var collection = GetMongodbDataBase().GetCollection<T>(collectionName);
+                return collection.Find(new BsonDocument()).ToList<T>();
+            }
+            catch (Exception ex) { throw new Exception(ex.Message); }
+        }
+    }
+}
---
+++ Mongodb.Test/Program.cs
@@ -0,0 +1,50 @@
+﻿using Mongodb.Common;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mongodb.Test
+{
+    class Program
+    {
+        public static string ExecDateDiff(DateTime dateBegin, DateTime dateEnd)
+        {
+            TimeSpan timeSpanBengin = new TimeSpan(dateBegin.Ticks);
+            TimeSpan timeSpanEnd = new TimeSpan(dateEnd.Ticks);
+            TimeSpan timeSpan = timeSpanBengin.Subtract(timeSpanEnd).Duration();
+            return timeSpan.TotalMilliseconds.ToString();
+        }
+
+        static void Main(string[] args)
+        {
+            string serverHost = "mongodb://sure:HUANGsl@localhost/Sure_mongodbStudy";
+            string databaseName = "Sure_mongodbStudy";
+            string collectionName = "Sure_mongodbStudy";
+            DateTime bengin = DateTime.Now;
+            Console.WriteLine("开始执行....");
+            //for (int i = 1; i <= 10000000; i++)
+            //{
+            //    Details detail = new Details() { name = $"黄森霖-{i}", age = i, sex = i % 2 == 0 ? "男" : "女" };
+            //    new MongodbHelper<Details>(serverHost, databaseName, collectionName).Insert(detail);
+            //}
+            List<Details> list = new MongodbHelper<Details>(serverHost, databaseName, collectionName).FindAll();
+            DateTime end = DateTime.Now;
+
+            Console.WriteLine("耗时：" + ExecDateDiff(bengin, end));
+        }
+    }
+
+    public class Details
+    {
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string id { set; get; }
+        public string name { set; get; }
+        public int age { set; get; }
+        public string sex { set; get; }
+
+    }
+}
